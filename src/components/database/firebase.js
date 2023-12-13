47ff1147c703f917a70a3e44e1ca1e4ac78dde92
Abzal <104160021+abzaliosdep@users.noreply.github.com>
2023-12-13 23:30:12 +0600

    Add files via upload

--- src/components/database/firebase.js
+++ src/components/database/firebase.js
@@ -1,22 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAqkIiZLmxBkOnOXOOGM4KrXv8aDcCy5_s",
-  authDomain: "otaumart-bf6ef.firebaseapp.com",
-  databaseURL: "https://otaumart-bf6ef-default-rtdb.firebaseio.com",
-  projectId: "otaumart-bf6ef",
-  storageBucket: "otaumart-bf6ef.appspot.com",
-  messagingSenderId: "461226652669",
-  appId: "1:461226652669:web:2e612cbbb15994ee3edf94",
-  measurementId: "G-6B42F0PRBB"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID" // Опционально для Firebase JS SDK v7.20.0 и выше
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app); // Инициализация Firestore
+
+export default db; // Экспорт для использования в других файлах
