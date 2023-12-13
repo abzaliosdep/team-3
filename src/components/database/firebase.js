6f681c6fdb497d3953361ea1cc15bf25bf472cab
Abzal <104160021+abzaliosdep@users.noreply.github.com>
2023-12-13 22:07:52 +0600

    Add files via upload

--- src/components/database/firebase.js
+++ src/components/database/firebase.js
@@ -1,6 +1,11 @@
-import {initializeApp} from "firebase/app";
-import {getFirestore} from "firebase/firestore";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAqkIiZLmxBkOnOXOOGM4KrXv8aDcCy5_s",
   authDomain: "otaumart-bf6ef.firebaseapp.com",
@@ -8,14 +13,10 @@ const firebaseConfig = {
   projectId: "otaumart-bf6ef",
   storageBucket: "otaumart-bf6ef.appspot.com",
   messagingSenderId: "461226652669",
-  appId: "1:461226652669:web:565918a95c0cca233edf94",
-  measurementId: "G-52EQ4GMC33"
-}
+  appId: "1:461226652669:web:2e612cbbb15994ee3edf94",
+  measurementId: "G-6B42F0PRBB"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
-
-export default db;
+const analytics = getAnalytics(app);
