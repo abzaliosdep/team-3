a766e6d92b841741dc7972eb3907a8da72dab236
Abzal <104160021+abzaliosdep@users.noreply.github.com>
2023-12-14 20:59:44 +0600

    Add files via upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,7 @@ import TextField from '@mui/material/TextField';
 import './Home.css';
 import albert from './../albert-4.png';
 import albertResult from './../albert.png';
-import {
-    doc,
-    collection,
-    runTransaction,
-} from "firebase/firestore";
+import { doc, collection, runTransaction } from "firebase/firestore";
 import {} from 'firebase/analytics'
 import db from "./database/firebase";
 import FormControl from "@mui/material/FormControl";
@@ -28,7 +24,7 @@ function Home() {
     });
 
     const addTeam = async () => {
-        const totalCommandsNumberRef = doc(db, 'total_commands', 'number');
+        const totalCommandsNumberRef = doc(db, 'total_people', 'number');
         const commandRef = collection(db, "commands");
 
         try {
@@ -52,8 +48,6 @@ function Home() {
                 transaction.update(totalCommandsNumberRef, { commands_number: commandNumber });
 
                 setCommandNumber(commandNumber);
-            }).then(() => {
-
             });
         } catch (e) {
             console.error("Ошибка: \n", e);
@@ -69,68 +63,66 @@ function Home() {
         setPhone('');
     }
 
-    if (commandNumber === 0) {
-        return <div className="home">
+    return (
+        <div className="home">
             <div className="card">
                 <div className="image">
                     <img src={albert} alt="Logo" />
                 </div>
-                <h1>Оставить заявку</h1>
-                <FormControl fullWidth sx={{ m: 1 }}>
-                    <ThemeProvider theme={theme}>
-                        <TextField
-                            className="text-field"
-                            id="outlined-adornment-amount"
-                            label="Имя"
-                            size="small"
-                            value={name}
-                            onChange={(e) => setName(e.target.value.trim())}
-                            required
-                            error={!Boolean(name)}
-                            helperText={!Boolean(name) ? "Поле 'Имя' необходимо заполнить" : ""}
-                        />
-                    </ThemeProvider>
-                </FormControl>
-                <FormControl fullWidth sx={{ m: 1 }}>
-                    <ThemeProvider theme={theme}>
-                        <TextField
-                            className="text-field"
-                            id="outlined-adornment-amount"
-                            label="Телефон"
-                            size="small"
-                            required
-                            type="number"
-                            placeholder="87759844789"
-                            value={phone}
-                            onChange={(e) => setPhone(e.target.value)}
-                            error={!Boolean(phone)}
-                            helperText={!Boolean(phone) ? "Поле 'Телефон' необходимо заполнить" : ""}
-                        />
-                    </ThemeProvider>
-                </FormControl>
-                <button disabled={!Boolean(name) || !Boolean(phone) || loading} onClick={addTeam}>
-                    {loading ? 'Отправка...' : "Отправить"}
-                </button>
-                <sup>Нажимая на кнопку, вы соглашаетесь на обработку персональных данных</sup>
+                {commandNumber === 0 ? (
+                    <div>
+                        <h1>Оставить заявку</h1>
+                        <FormControl fullWidth sx={{ m: 1 }}>
+                            <ThemeProvider theme={theme}>
+                                <TextField
+                                    className="text-field"
+                                    id="outlined-adornment-amount"
+                                    label="Имя"
+                                    size="small"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value.trim())}
+                                    required
+                                    error={!Boolean(name)}
+                                    helperText={!Boolean(name) ? "Поле 'Имя' необходимо заполнить" : ""}
+                                />
+                            </ThemeProvider>
+                        </FormControl>
+                        <FormControl fullWidth sx={{ m: 1 }}>
+                            <ThemeProvider theme={theme}>
+                                <TextField
+                                    className="text-field"
+                                    id="outlined-adornment-amount"
+                                    label="Телефон"
+                                    size="small"
+                                    required
+                                    type="number"
+                                    placeholder="87759844789"
+                                    value={phone}
+                                    onChange={(e) => setPhone(e.target.value)}
+                                    error={!Boolean(phone)}
+                                    helperText={!Boolean(phone) ? "Поле 'Телефон' необходимо заполнить" : ""}
+                                />
+                            </ThemeProvider>
+                        </FormControl>
+                        <button disabled={!Boolean(name) || !Boolean(phone) || loading} onClick={addTeam}>
+                            {loading ? 'Отправка...' : "Отправить"}
+                        </button>
+                        <sup>Нажимая на кнопку, вы соглашаетесь на обработку персональных данных</sup>
+                    </div>
+                ) : (
+                    <div className="text-center">
+                        <h1>Ваш номер: <span className="red-text">{commandNumber}</span></h1>
+                        <h3 className="red-text">Пожалуйста, сделайте скрин экрана.</h3>
+                        <img src={albertResult} alt="" className="albert-image" />
+                        <button onClick={comeBack}>
+                            Вернуться назад
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
-    } else {
-        return <div className="home">
-            <div className="card">
-                <div className="image">
-                    <img src={albert} alt="Logo" />
-                </div>
-                <div className="text-center">
-                    <h1>Ваш номер: <span className="red-text">{commandNumber}</span></h1>
-                    <h3 className="red-text">Пожалуйста, сделайте скрин экрана.</h3>
-                    <img src={albertResult} alt="" className="albert-image" />
-                </div>
-                <button onClick={comeBack}>
-                    Вернуться назад
-                </button>
-            </div>
-        </div>
-    }
+    );
 }
 
 export default Home;
+
